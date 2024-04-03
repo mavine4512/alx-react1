d5d112e5a341ba57b47808798278acc518acde32
mavine4512 <mavineoyugi@gmail.com>
2024-04-03 08:47:00 +0300

    file clean up

--- 0x00-Webpack/task_0/src/main.js
+++
@@ -1,7 +0,0 @@
-// src/main.js
-import $ from "jquery";
-
-// Add elements to the body using jQuery
-$(document).ready(function () {
-  $("body").append("<h1>Hello, Webpack with jQuery!</h1>");
-});
