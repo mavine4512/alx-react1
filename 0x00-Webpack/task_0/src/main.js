967da1e5bd8fe4489158572f86920a5ee00247aa
mavine4512 <mavineoyugi@gmail.com>
2024-04-03 08:29:44 +0300

    fixed item

---
+++ 0x00-Webpack/task_0/src/main.js
@@ -0,0 +1,7 @@
+// src/main.js
+import $ from "jquery";
+
+// Add elements to the body using jQuery
+$(document).ready(function () {
+  $("body").append("<h1>Hello, Webpack with jQuery!</h1>");
+});
