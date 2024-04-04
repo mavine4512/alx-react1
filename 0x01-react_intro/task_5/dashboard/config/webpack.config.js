5043314038b0126ecb77de869dfcf8f5616539d4
mavine4512 <mavineoyugi@gmail.com>
2024-04-04 03:00:17 +0300

    fixed items

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./dashboard/src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "../dist"),
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+};
