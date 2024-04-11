fb022b31be627b0fcbb73a465fce40ef2791403f
mavine4512 <mavineoyugi@gmail.com>
2024-04-12 00:47:57 +0300

    fixed item

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   };
 
   render() {
-    const { isLoggedIn } = this.state;
+    const { isLoggedIn } = this.props;
     return (
       <>
         <div className="App">
