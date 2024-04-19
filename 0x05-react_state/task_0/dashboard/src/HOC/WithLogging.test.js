d3ffd5d1cff914898e16e5d151ad85ee50a7a185
mavine4512 <mavineoyugi@gmail.com>
2024-04-19 23:22:02 +0300

    fixed app.js

---
+++ 0x05-react_state/task_0/dashboard/src/HOC/WithLogging.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { mount } from "enzyme";
+import { jest } from "@jest/globals";
+
+import WithLogging from "./WithLogging";
+import Login from "../Login/Login";
+
+describe("Testing WithLogging HOC", () => {
+  it("should make sure console logs was called on unmount and mount with component when the wrapper element is pure html", () => {
+    console.log = jest.fn();
+    const Hoc = WithLogging(() => <p>Hello there</p>);
+    const comp = <Hoc title="hello" />;
+    let wrapper = mount(comp);
+
+    expect(console.log).toBeCalleWith("Component is mounted");
+    wrapper.unmount();
+    expect(console.log).toBeCalleWith("Component is going to unmounted");
+    jest.restoreAllMocks();
+  });
+});
