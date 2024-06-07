ccde7e138f18f05bbd9c22138edcf1906c47ec21
mavine4512 <mavineoyugi@gmail.com>
2024-06-07 22:27:07 +0300

    fixed code

--- 0x08-react_redux_reducer_selector/task_0/dashboard/src/App/App.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/App/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import Notifications from '../Notifications/Notifications';
-import CourseList from '../CourseList/CourseList';
-import BodySection from '../BodySection/BodySection';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
-import PropTypes from 'prop-types';
-import { getLatestNotification } from '../utils/utils';
-import { StyleSheet, css } from 'aphrodite';
-import { user, logOut, AppContext } from './AppContext';
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Notifications from "../Notifications/Notifications";
+import CourseList from "../CourseList/CourseList";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
+import { StyleSheet, css } from "aphrodite";
+import { user, logOut, AppContext } from "./AppContext";
 
 const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
 ];
 export const listNotificationsInitialState = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
 ];
 
 class App extends Component {
@@ -39,14 +39,14 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    window.addEventListener('keydown', this.handleLogout);
+    window.addEventListener("keydown", this.handleLogout);
   }
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleLogout);
+    window.removeEventListener("keydown", this.handleLogout);
   }
   handleLogout(e) {
-    if (e.key === 'h' && e.ctrlKey) {
-      alert('Logging you out');
+    if (e.key === "h" && e.ctrlKey) {
+      alert("Logging you out");
       this.state.logOut();
     }
   }
@@ -96,15 +96,15 @@ class App extends Component {
         />
         <Header />
         {isLoggedIn ? (
-          <BodySectionWithMarginBottom title='Course list'>
+          <BodySectionWithMarginBottom title="Course list">
             <CourseList listCourses={listCourses} />
           </BodySectionWithMarginBottom>
         ) : (
-          <BodySectionWithMarginBottom title='Log in to continue'>
+          <BodySectionWithMarginBottom title="Log in to continue">
             <Login />
           </BodySectionWithMarginBottom>
         )}
-        <BodySection title='News from the School'>
+        <BodySection title="News from the School">
           <p className={css(styles.p)}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
             ullam? Quisquam eos temporibus, voluptate error, sunt consectetur
@@ -121,17 +121,16 @@ class App extends Component {
 }
 
 App.defaultProps = {};
-
 App.propTypes = {};
 
 const styles = StyleSheet.create({
   footer: {
-    width: '100%',
-    position: 'fixed',
+    width: "100%",
+    position: "fixed",
     bottom: 0,
-    textAlign: 'center',
-    fontStyle: 'italic',
-    borderTop: 'thick solid #e0344a',
+    textAlign: "center",
+    fontStyle: "italic",
+    borderTop: "thick solid #e0344a",
   },
   p: {
     marginTop: 0,
