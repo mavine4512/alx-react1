7fead2abba5879360ef74da95ddb0c5764346afa
mavine4512 <mavineoyugi@gmail.com>
2024-07-16 23:36:24 +0300

    fixed items

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -3,7 +3,7 @@ import { normalize, schema } from "normalizr";
 
 const user = new schema.Entity("users");
 const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
-const notification = new schema.Entity("notification", {
+const notification = new schema.Entity("notifications", {
   author: user,
   context: message,
 });
